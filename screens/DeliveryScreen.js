786db6d8307e23bcac120c5274b23b8b27dee23d
Mohammed Surguli <Mohammedsorguli@gmail.com>
2023-11-09 15:09:06 +0100

    Deleted: Google Routes, Deelted:Google Time Calculation, Added: Straight Line Route, Added: Distance Measeure in KM,

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -1,88 +1,43 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, Image, SafeAreaView } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { View, Text, Image, SafeAreaView } from 'react-native';
 import { useSelector } from 'react-redux';
 import { selectRestaurant } from '../features/restaurantSlice';
-import { XIcon } from 'react-native-heroicons/solid';
 import * as Progress from 'react-native-progress';
 import MapView, { Marker, Polyline } from 'react-native-maps';
-import axios from 'axios';
-
 
 const DeliveryScreen = () => {
-    const navigation = useNavigation();
     const restaurant = useSelector(selectRestaurant);
-    const destination = { latitude: 52.50768847543942, longitude: 13.294998226071066 };
-    const [routeCoordinates, setRouteCoordinates] = useState([]);
-    const [estimatedArrival, setEstimatedArrival] = useState('Calculating...');
+    const homeLocation = { latitude: 52.50768847543942, longitude: 13.294998226071066 };
+    const [distance, setDistance] = useState('Calculating...');
 
     useEffect(() => {
-        // Replace with your Google Maps Directions API key
-        const apiKey = 'AIzaSyBU0AskPr6__PQEEllCpvJARY0DUjE3_Vs';
+        // Calculate the distance between restaurant and home location using the Haversine formula
+        const calculateDistance = () => {
+            const earthRadius = 6371; // Earth's radius in kilometers
+            const lat1 = restaurant.lat;
+            const lon1 = restaurant.long;
+            const lat2 = homeLocation.latitude;
+            const lon2 = homeLocation.longitude;
 
-        // Fetch driving route data
-        axios
-            .get(
-                `https://maps.googleapis.com/maps/api/directions/json?origin=${restaurant.lat},${restaurant.long}&destination=${destination.latitude},${destination.longitude}&mode=driving&key=${apiKey}`
-            )
-            .then((response) => {
-                const route = response.data.routes[0];
-                const overviewPolyline = route.overview_polyline.points;
-                const decodedPoints = decodePolyline(overviewPolyline);
-                setRouteCoordinates(decodedPoints);
+            const dLat = (lat2 - lat1) * (Math.PI / 180);
+            const dLon = (lon2 - lon1) * (Math.PI / 180);
 
-                // Calculate estimated arrival time
-                const duration = route.legs[0].duration.text;
-                setEstimatedArrival(duration);
-            })
-            .catch((error) => {
-                console.error('Error fetching route data:', error);
-            });
-    }, [restaurant, destination]);
+            const a =
+                Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(lat1 * (Math.PI / 180)) * Math.cos(lat2 * (Math.PI / 180)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
 
-    // Decode Google Maps Polyline
-  // Decode Google Maps Polyline
-function decodePolyline(encoded) {
-    var poly = [];
-    var index = 0, len = encoded.length;
-    var lat = 0, lng = 0;
-  
-    while (index < len) {
-      var b, shift = 0, result = 0;
-  
-      do {
-        b = encoded.charCodeAt(index++) - 63;
-        result |= (b & 0x1f) << shift;
-        shift += 5;
-      } while (b >= 0x20);
-  
-      var dlat = ((result & 1) !== 0 ? ~(result >> 1) : (result >> 1));
-      lat += dlat;
-  
-      shift = 0;
-      result = 0;
-  
-      do {
-        b = encoded.charCodeAt(index++) - 63;
-        result |= (b & 0x1f) << shift;
-        shift += 5;
-      } while (b >= 0x20);
-  
-      var dlng = ((result & 1) !== 0 ? ~(result >> 1) : (result >> 1));
-      lng += dlng;
-  
-      var point = { latitude: lat / 1e5, longitude: lng / 1e5 };
-      poly.push(point);
-    }
-  
-    return poly;
-  }
-  
+            const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+            const calculatedDistance = earthRadius * c;
+
+            // Update the state with the calculated distance
+            setDistance(calculatedDistance.toFixed(2) + ' km');
+        };
+
+        calculateDistance();
+    }, [restaurant, homeLocation]);
 
     return (
         <View style={{ flex: 1, backgroundColor: '#00CCBB' }}>
-           
-
             <MapView
                 initialRegion={{
                     latitude: restaurant.lat,
@@ -91,46 +46,36 @@ function decodePolyline(encoded) {
                     longitudeDelta: 0.05,
                 }}
                 style={{ flex: 1, marginTop: -10, zIndex: 0 }}
-                mapType='mutedStandard'
+                mapType="mutedStandard"
             >
-                {routeCoordinates.length > 0 ? (
-                <>
-                <Marker
-                coordinate={routeCoordinates[0]}
-                title={restaurant.title}
-                description={restaurant.short_description}
+                <Polyline
+                    coordinates={[
+                        { latitude: restaurant.lat, longitude: restaurant.long },
+                        homeLocation,
+                    ]}
+                    strokeColor="#ee5253"
+                    strokeWidth={6}
                 />
+                <Marker coordinate={homeLocation} pinColor="#ee5253" />
                 <Marker
-                coordinate={routeCoordinates[routeCoordinates.length - 1]}
-                pinColor='#ee5253'
+                    coordinate={{ latitude: restaurant.lat, longitude: restaurant.long }}
+                    title={restaurant.title}
+                    description={restaurant.short_description}
                 />
-                <Polyline coordinates={routeCoordinates} strokeColor="#ee5253" strokeWidth={6} />
-                </>
-                ) : null}
-
             </MapView>
 
-
-            <SafeAreaView style={{ backgroundColor: 'white', flexDirection: 'row', alignItems: 'center', paddingHorizontal: 10, height: 100 }}>
+            <SafeAreaView style={{ backgroundColor: 'white', flexDirection: 'row', alignItems: 'center', paddingHorizontal: 10, height: 130 }}>
+                <View>
+                    <Text style={{ fontSize: 14, color: 'gray' , paddingLeft: 5}}>Distance to Restaurant</Text>
+                    <Text style={{ fontSize: 18, fontWeight: 'bold', paddingLeft: 5 }}>{distance}</Text>
+                </View> 
+                
+                <Progress.Bar size={40} color="#00CCBB" indeterminate={true} />
+                <Image source={{ uri: 'https://links.papareact.com/fls' }} style={{ width: 100, height: 100 }} />
+             
+            </SafeAreaView>
             
-                  
-            <View>
-            <Text style={{ fontSize: 14, color: 'gray' }}>Estimated Arrival</Text>
-                    {<Text style={{ fontSize: 18, fontWeight: 'bold'}}>{estimatedArrival}</Text>}
-                </View>
-                {<Progress.Bar size={40} color="#00CCBB" indeterminate={true}  style={{marginLeft:20}}/>}
-                { <Image source={{ uri: 'https://links.papareact.com/fls' }} style={{ width: 100, height: 100 }} />}
-
-            </SafeAreaView>
-
-            <SafeAreaView style={{ backgroundColor: 'white', flexDirection: 'row', alignItems: 'center', paddingHorizontal: 10, height: 100 }}>
-                <Image source={{ uri: 'https://links.papareact.com/wru' }} style={{ width: 60, height: 60, backgroundColor: 'gray', padding: 15, borderRadius: 100, marginLeft: 15 }} />
-                <View style={{ flex: 1 }}>
-                    <Text style={{ fontSize: 20 }}> Mohammed</Text>
-                    <Text style={{ color: 'gray' }}> Your Rider</Text>
-                </View>
-                <Text style={{ color: '#00CCBB', fontSize: 20, marginRight: 15, fontWeight: 'bold' }}>Call</Text>
-            </SafeAreaView>
+            {/* Additional UI elements for the rider information */}
         </View>
     );
 };
